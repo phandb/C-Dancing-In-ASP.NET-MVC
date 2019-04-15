8e988a050c08982390d07171eab80c1e7a6dd723
Dalton <phandb72@gmail.com>
2019-04-15 01:09:33 -0500

    Add project files.

---
+++ PatientApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PatientApplication.Startup))]
+namespace PatientApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
