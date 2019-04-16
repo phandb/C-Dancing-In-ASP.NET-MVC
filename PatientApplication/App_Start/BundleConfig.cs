f439b05f6b91666bb11e917234a090d412a9711a
Dalton <phandb72@gmail.com>
2019-04-15 23:15:15 -0500

    Change theme to pulse.  reconstruct navbar.  Partial view NavBar

--- PatientApplication/App_Start/BundleConfig.cs
+++ PatientApplication/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace PatientApplication
                       "~/Scripts/umd/bootstrap.js"));
             
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/cyborg.bootstrap.css",
+                      "~/Content/pulse.bootstrap.css",
                       "~/Content/site.css"));
         }
     }
