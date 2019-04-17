3a7d5a131d93356d39419085fd3057eb74756987
Dalton <phandb72@gmail.com>
2019-04-17 00:00:07 -0500

    Add Patient model.  Add DbSet<Patient> to IdentityModels

---
+++ PatientApplication/Models/Patient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PatientApplication.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        public DateTime? Birthdate { get; set; }
+
+        public string Address { get; set; }
+
+
+    }
+}
